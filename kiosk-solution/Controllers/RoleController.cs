fe3bab16f6de2008d6cbf9c5f367af9a7c543505
tung <thanhgodlike@gmail.com>
2022-05-14 00:26:45 +0700

    update use-roleId  + api getAllRole

---
+++ kiosk-solution/Controllers/RoleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using kiosk_solution.Business.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace kiosk_solution.Controllers
+{
+    [Route("api/v{version:apiVersion}/roles")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class RoleController : Controller
+    {
+        private readonly IRoleService _roleService;
+        private IConfiguration _configuration;
+        public RoleController(IRoleService roleService, IConfiguration configuration)
+        {
+            _roleService = roleService;
+            _configuration = configuration;
+        }
+        
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [MapToApiVersion("1")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _roleService.GetAll());
+        }
+    }
+}
