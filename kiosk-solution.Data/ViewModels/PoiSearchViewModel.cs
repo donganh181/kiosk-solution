302cec3b75466ef29ab21589f1e9fdcd3465f645
tung <thanhgodlike@gmail.com>
2022-06-07 15:49:59 +0700

    get POI (not test yet)

---
+++ kiosk-solution.Data/ViewModels/PoiSearchViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using kiosk_solution.Data.Attributes;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace kiosk_solution.Data.ViewModels
+{
+    public class PoiSearchViewModel
+    {
+        [BindNever]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        [BindNever]
+        public string Description { get; set; }
+        [BindNever]
+        public TimeSpan? OpenTime { get; set; }
+        [BindNever]
+        public string DayOfWeek { get; set; }
+        [BindNever]
+        public string Longtitude { get; set; }
+        [BindNever]
+        public string Latitude { get; set; }
+        [String]
+        public string Ward { get; set; }
+        [String]
+        public string District { get; set; }
+        [String]
+        public string City { get; set; }
+        [String]
+        public string Address { get; set; }
+        [BindNever]
+        public DateTime? CreateDate { get; set; }
+        [BindNever]
+        public Guid? CreatorId { get; set; }
+        [BindNever]
+        public string Status { get; set; }
+        [BindNever]
+        public string Type { get; set; }
+    }
+}
