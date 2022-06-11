6cd389a7df5f9049e3d0ad704e37e992cdcc5f53
phamlong992k@gmail.com <phamlong992k@gmail.com>
2022-06-11 23:01:34 +0700

    update

--- kiosk-solution/Controllers/RoleController.cs
+++ kiosk-solution/Controllers/RoleController.cs
@@ -31,13 +31,5 @@ namespace kiosk_solution.Controllers
             var list = await _roleService.GetAll();
             return Ok(new SuccessResponse<List<RoleViewModel>>((int)HttpStatusCode.OK, "Found.", list));
         }
-        
-        [HttpGet("test")]
-        [MapToApiVersion("1")]
-        public async Task<IActionResult> Test()
-        {
-            var list = await _roleService.GetAll();
-            return Ok(new SuccessResponse<List<RoleViewModel>>((int)HttpStatusCode.OK, "Found.", list));
-        }
     }
 }
