5f0daa63fd4132ff49c5b3159be14fcceb251ab6
ANHTTNSE62395\ADMIN <nhatanh2321997@gmail.com>
2022-06-14 13:35:24 +0700

    add add image to POI api (not test)

--- kiosk-solution.Business/Services/impl/PoiService.cs
+++ kiosk-solution.Business/Services/impl/PoiService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using kiosk_solution.Data.Models;
 using kiosk_solution.Data.Repositories;
 using kiosk_solution.Data.Responses;
 using kiosk_solution.Data.ViewModels;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace kiosk_solution.Business.Services.impl
@@ -20,17 +22,21 @@ namespace kiosk_solution.Business.Services.impl
         private readonly IUnitOfWork _unitOfWork;
         private readonly ILogger<IPoiService> _logger;
         private readonly IMapService _mapService;
+        private readonly IImageService _imageService;
 
-        public PoiService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<IPoiService> logger, IMapService mapService)
+        public PoiService(IUnitOfWork unitOfWork, IMapper mapper,
+            ILogger<IPoiService> logger, IMapService mapService, IImageService imageService)
         {
             _mapper = mapper;
             _unitOfWork = unitOfWork;
             _logger = logger;
             _mapService = mapService;
+            _imageService = imageService;
         }
 
         public async Task<PoiViewModel> Create(Guid partyId, string roleName, PoiCreateViewModel model)
         {
+            List<ImageViewModel> listPoiImage = new List<ImageViewModel>();
             var poi = _mapper.Map<Poi>(model);
             var address = $"{poi.Address}, {poi.Ward}, {poi.District}, {poi.City}";
             var geoCodeing = await _mapService.GetForwardGeocode(address);
@@ -49,7 +55,28 @@ namespace kiosk_solution.Business.Services.impl
             {
                 await _unitOfWork.PoiRepository.InsertAsync(poi);
                 await _unitOfWork.SaveAsync();
-                var result = _mapper.Map<PoiViewModel>(poi);
+
+                ImageCreateViewModel thumbnailModel = new ImageCreateViewModel(poi.Name,
+                    model.Thumbnail, poi.Id, CommonConstants.POI_IMAGE, CommonConstants.THUMBNAIL);
+
+                var thumbnail = await _imageService.Create(thumbnailModel);
+
+                var result = await _unitOfWork.PoiRepository
+                    .Get(p => p.Id.Equals(poi.Id))
+                    .Include(p => p.Creator)
+                    .Include(p => p.Poicategory)
+                    .ProjectTo<PoiViewModel>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync();
+                foreach(var img in model.ListImage)
+                {
+                    ImageCreateViewModel imageModel = new ImageCreateViewModel(result.Name, img.Image,
+                    result.Id, CommonConstants.EVENT_IMAGE, CommonConstants.SOURCE_IMAGE);
+                    var image = await _imageService.Create(imageModel);
+                    listPoiImage.Add(image);
+                }
+                var poiImage = _mapper.Map<List<PoiImageDetailViewModel>>(listPoiImage);
+                result.Thumbnail = thumbnail;
+                result.ListImage = poiImage;
                 return result;
             }
             catch (Exception)
---
+++ kiosk-solution.Data/ViewModels/PoiAddImageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiosk_solution.Data.ViewModels
+{
+    public class PoiAddImageViewModel
+    {
+        public Guid Id { get; set; }
+        public List<PoiImageDetailCreateViewModel> ListImage { get; set; }
+    }
+    public class PoiImageDetailCreateViewModel
+    {
+        public string Image { get; set; }
+    }
+}
