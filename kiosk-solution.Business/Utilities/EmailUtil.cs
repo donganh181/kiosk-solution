fc0ec160331d1257e58542c8b710c872e852d455
phamlong992k@gmail.com <phamlong992k@gmail.com>
2022-06-20 15:47:52 +0700

    update send email by sendgrid

--- kiosk-solution.Business/Utilities/EmailUtil.cs
+++ kiosk-solution.Business/Utilities/EmailUtil.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using kiosk_solution.Data.Constants;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace kiosk_solution.Data.ViewModels
 {
@@ -12,37 +16,65 @@ namespace kiosk_solution.Data.ViewModels
 
         public static async Task SendEmail(string sendto, string subject, string content)
         {
-            //sendto: Email receiver (người nhận)
-            //subject: Tiêu đề email
-            //content: Nội dung của email, bạn có thể viết mã HTML
-            //Nếu gửi email thành công, sẽ trả về kết quả: OK, không thành công sẽ trả về thông tin l�-i
-            try
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("capstoneprojectfu2021@gmail.com", "Kiosk-Platform");
+            var to = new EmailAddress(sendto, sendto);
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = content;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            switch (response.StatusCode)
             {
-                MailMessage mail = new MailMessage();
-                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
-
-                mail.From = new MailAddress(_email);
-                mail.To.Add(sendto);
-                mail.Subject = subject;
-                mail.IsBodyHtml = true;
-                mail.Body = content;
-
-                smtpServer.Host = "smtp.gmail.com";
-                smtpServer.Port = 587;
-                smtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtpServer.EnableSsl = true;
-                smtpServer.UseDefaultCredentials = false;
-                smtpServer.Credentials = new System.Net.NetworkCredential(_email, _pass);
-
-                await smtpServer.SendMailAsync(mail);
-                Console.WriteLine("Email sent successfully");
+                case HttpStatusCode.Accepted:
+                case HttpStatusCode.OK:
+                    Console.WriteLine("Send email success");
+                    break;
             }
-            catch (Exception ex)
+            var body = string.Empty;
+            using (var reader = new StreamReader(response.Body.ReadAsStream()))
             {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("Email sending failed");
+                //Request.Body.Seek(0, SeekOrigin.Begin);
+                //body = reader.ReadToEnd();
+                body = reader.ReadToEnd();
             }
+
+            Console.WriteLine(body);
         }
+
+        // public static async Task SendEmail(string sendto, string subject, string content)
+        // {
+        //     //sendto: Email receiver (người nhận)
+        //     //subject: Tiêu đề email
+        //     //content: Nội dung của email, bạn có thể viết mã HTML
+        //     //Nếu gửi email thành công, sẽ trả về kết quả: OK, không thành công sẽ trả về thông tin l�-i
+        //     try
+        //     {
+        //         MailMessage mail = new MailMessage();
+        //         SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        //
+        //         mail.From = new MailAddress(_email);
+        //         mail.To.Add(sendto);
+        //         mail.Subject = subject;
+        //         mail.IsBodyHtml = true;
+        //         mail.Body = content;
+        //
+        //         smtpServer.Host = "smtp.gmail.com";
+        //         smtpServer.Port = 587;
+        //         smtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+        //         smtpServer.EnableSsl = true;
+        //         smtpServer.UseDefaultCredentials = false;
+        //         smtpServer.Credentials = new System.Net.NetworkCredential(_email, _pass);
+        //
+        //         await smtpServer.SendMailAsync(mail);
+        //         Console.WriteLine("Email sent successfully");
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Console.WriteLine(ex.Message);
+        //         Console.WriteLine("Email sending failed");
+        //     }
+        // }
 
         public static string getCreateAccountContent(string email)
         {
@@ -50,6 +82,7 @@ namespace kiosk_solution.Data.ViewModels
                 .Replace("PASSWORD", DefaultConstants.DEFAULT_PASSWORD);
             return result;
         }
+
         public static string getCreateKioskContent(string email)
         {
             string result = EmailConstants.CREATE_KIOSK_CONTENT.Replace("EMAIL", email);
