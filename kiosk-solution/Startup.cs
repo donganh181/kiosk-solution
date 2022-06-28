dbfa8a50f03f76eab71e88f2d6588ea64a3bb6b9
phamlong992k@gmail.com <phamlong992k@gmail.com>
2022-06-28 15:06:06 +0700

    cron schedule

--- kiosk-solution/Startup.cs
+++ kiosk-solution/Startup.cs
@@ -81,7 +81,7 @@ namespace kiosk_solution
                 q.ScheduleJob<CheckTemplateJob>(trigger => trigger
                     .WithIdentity("Combined Configuration Trigger")
                     .StartAt(DateBuilder.EvenSecondDate(DateTimeOffset.UtcNow.AddSeconds(7)))
-                    .WithDailyTimeIntervalSchedule(x => x.WithInterval(10, IntervalUnit.Second))
+                    .WithCronSchedule("* * * ? * *")
                     .WithDescription("my awesome trigger configured for a job with single call")
                 );
             });
