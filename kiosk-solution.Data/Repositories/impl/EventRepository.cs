ac53ae0e60f690de4bef5a533df75bc6d5fd6706
anhttd <anhttdse140078@fpt.edu.vn>
2022-07-21 21:42:48 +0700

    add get event nearby api

--- kiosk-solution.Data/Repositories/impl/EventRepository.cs
+++ kiosk-solution.Data/Repositories/impl/EventRepository.cs
@@ -1,4 +1,5 @@
-﻿using kiosk_solution.Data.Context;
+﻿using kiosk_solution.Data.Constants;
+using kiosk_solution.Data.Context;
 using kiosk_solution.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -14,5 +15,20 @@ namespace kiosk_solution.Data.Repositories.impl
         public EventRepository(Kiosk_PlatformContext dbContext) : base(dbContext)
         {
         }
+        public IQueryable<Event> GetEventNearBy(Guid partyId, double longitude, double latitude)
+        {
+            var result = dbContext.Events.Where(x =>
+                            (Math.Sqrt(Math.Pow(69.1 * (latitude - (double)x.Latitude), 2) +
+                            Math.Pow(69.1 * (double)(x.Longtitude - longitude) * Math.Cos(latitude / 57.3), 2))) * 1.609344 < 5
+                            && (x.Type.Equals(TypeConstants.SERVER_TYPE) || (x.Type.Equals(TypeConstants.LOCAL_TYPE) && x.CreatorId.Equals(partyId))
+                            && (x.Status.Equals(StatusConstants.ON_GOING) || x.Status.Equals(StatusConstants.COMING_SOON))))
+
+                        .OrderByDescending(x =>
+                            (Math.Sqrt(Math.Pow(69.1 * (latitude - (double)x.Latitude), 2) +
+                            Math.Pow(69.1 * (double)(x.Longtitude - longitude) * Math.Cos(latitude / 57.3), 2))) * 1.609344);
+
+            return result;
+        }
+
     }
 }
