60f16779e5c137c0c8afee63df2716b34b58a573
phamlong992k@gmail.com <phamlong992k@gmail.com>
2022-07-30 02:17:22 +0700

    update

--- kiosk-solution.Business/Hubs/SystemEventHub.cs
+++ kiosk-solution.Business/Hubs/SystemEventHub.cs
@@ -9,12 +9,16 @@ namespace kiosk_solution.Business.Hubs
     public class SystemEventHub : Hub
     {
         public static string KIOSK_CONNECTION_CHANNEL = "KIOSK_CONNECTION_CHANNEL";
+        public static string KIOSK_MESSAGE_CONNECTION_CHANNEL = "KIOSK_MESSAGE_CONNECTED_CHANNEL";
         public static string SYSTEM_BOT = "SYSTEM_BOT";
 
         public async Task JoinRoom(KioskConnectionViewModel kioskConnection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, kioskConnection.RoomId);
             Console.WriteLine($"{kioskConnection.KioskId} has joined {kioskConnection.RoomId}");
+            await Clients.Group(kioskConnection.KioskId)
+                .SendAsync(KIOSK_MESSAGE_CONNECTION_CHANNEL,
+                    SYSTEM_BOT, "Connected On Kiosk System Success");
         }
     }
 }
