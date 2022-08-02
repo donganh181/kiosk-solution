652438842f0eec50b6782811d73c55f65d097aa6
anhttd <anhttdse140078@fpt.edu.vn>
2022-08-02 20:40:49 +0700

    Add change status api for schedule

--- kiosk-solution.Data/Models/KioskScheduleTemplate.cs
+++ kiosk-solution.Data/Models/KioskScheduleTemplate.cs
@@ -11,6 +11,7 @@ namespace kiosk_solution.Data.Models
         public Guid? ScheduleId { get; set; }
         public Guid? TemplateId { get; set; }
         public Guid? KioskId { get; set; }
+        public string Status { get; set; }
 
         public virtual Kiosk Kiosk { get; set; }
         public virtual Schedule Schedule { get; set; }
